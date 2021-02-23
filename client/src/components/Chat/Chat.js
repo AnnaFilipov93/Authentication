49051c9a2e7aad6b427af30b7ae2bcc304ab411a
Anna Filipov <filipovanna93@gmail.com>
2021-02-23 16:29:46 +0200

    Added Join route

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
 
-const Chat = () => {
+const Chat = (location) => {
 
+    useEffect(() => {
+
+        const {room, name} = queryString.parse(location.search);
+
+        console.log(room);
+        console.log(name);
+    })
     return (
         <h1>Chat</h1>
     )
